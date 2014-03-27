94a37698ff655f62cd9e532ec993ba0392e02ba7
dillonweip <dillon00072002@gmail.com>
2014-03-27 11:01:00 +0800

    V 0.0.1

---
+++ js/tile.js
@@ -0,0 +1,29 @@
+function Tile(position, value) {
+  this.x                = position.x;
+  this.y                = position.y;
+  this.value            = value || "Mac OS";
+  this.score			= 2;
+
+  this.previousPosition = null;
+  this.mergedFrom       = null; // Tracks tiles that merged together
+}
+
+Tile.prototype.addScore=function(){
+  this.score*=2;
+}
+
+Tile.prototype.savePosition = function () {
+  this.previousPosition = { x: this.x, y: this.y };
+};
+
+Tile.prototype.updatePosition = function (position) {
+  this.x = position.x;
+  this.y = position.y;
+};
+
+Tile.prototype.clone = function() {
+  newTile = new Tile({ x: this.x, y: this.y }, this.value);
+  //newTile.previousPosition = { x: this.previousPosition.x, y: this.previousPosition.y };
+  //newTile.mergedFrom = { x: this.previousPosition.x, y: this.previousPosition.y };
+  return newTile;
+}
